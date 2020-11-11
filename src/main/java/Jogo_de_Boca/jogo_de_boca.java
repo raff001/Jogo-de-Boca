b4ce5d6649d99ce53df6fe4ff09559a6142a42c6
Rafael <rafaelcrd.ribeiro@gmail.com>
2020-11-11 20:00:04 -0300

    Update 1
    Correção gramatical e exclusão do int i que não estava sendo usado

--- src/main/java/Jogo_de_Boca/jogo_de_boca.java
+++ src/main/java/Jogo_de_Boca/jogo_de_boca.java
@@ -16,43 +16,43 @@ public class jogo_de_boca {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int i, jogo, rodada, n, valor;
+        int jogo, rodada, n, valor;
 
-        System.out.println("\n\n=== Jogo de Boca ===");
-        System.out.println("\nDigite o nÃºmero objetivo do jogo: ");
+        System.out.println("=== Jogo de Boca ===");
+        System.out.println("\nDigite o número objetivo do jogo: ");
         jogo = input.nextInt();
         valor = 0;
 
         while (valor < jogo) {
             for (rodada = 1; valor < jogo; rodada++) {
                 if (rodada % 2 != 0) {
-                    System.out.println("\n" + rodada + "Âª Rodada");
+                    System.out.println("\n" + rodada + "ª Rodada");
                     System.out.println("Primeiro Jogador digite 1 ou 2: ");
                     n = input.nextInt();
                     while ((n != 1) && (n != 2)) {
-                        System.out.println("NÃºmero invÃ¡lido, tente novamente: ");
+                        System.out.println("Número inválido, tente novamente: ");
                         n = input.nextInt();
                     }
                     valor = valor + n;
-                    System.out.println("A somatÃ³ria chegou em: " + valor);
+                    System.out.println("A somatória chegou em: " + valor);
 
                 } else {
-                    System.out.println("\n" + rodada + "Âª Rodada");
+                    System.out.println("\n" + rodada + "ª Rodada");
                     System.out.println("Segundo Jogador digite 1 ou 2: ");
                     n = input.nextInt();
                     while ((n != 1) && (n != 2)) {
-                        System.out.println("NÃºmero invÃ¡lido, tente novamente: ");
+                        System.out.println("Número inválido, tente novamente: ");
                         n = input.nextInt();
                     }
                     valor = valor + n;
-                    System.out.println("A somatÃ³ria chegou em: " + valor);
+                    System.out.println("A somatória chegou em: " + valor);
                 }
                 if ((valor == jogo) && (rodada % 2 != 0)) {
                     System.out.println("\nFim do Jogo!");
-                    System.out.println("Primeiro jogador venceu, parabÃ©ns! ");
+                    System.out.println("Primeiro jogador venceu, parabéns! ");
                 } else if ((valor == jogo) && (rodada % 2 == 0)) {
                     System.out.println("\nFim do Jogo!");
-                    System.out.println("Segundo jogador venceu, parabÃ©ns! ");
+                    System.out.println("Segundo jogador venceu, parabéns! ");
 
                 }
             }
